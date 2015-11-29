961792188251f886e35fde4ba406e55457a59178
Simon DENEL <simondenel1@gmail.com>
2015-11-29 21:58:46 +0100

    New version : customizable background + quotes + unstoppable chrono mode

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ HTMLcountdownTimer
 [![Screenshot](https://raw.githubusercontent.com/sdenel/HTMLcountdownTimer/master/inc/screenshot.png)][1]
 
 
-A simple HTML countdown timer, using AngularJS.
+A simple HTML countdown timer, using AngularJS. Useful to do the pomodoro technique as described in A Mind for Numbers: How to Excel at Math and Science (Even If You Flunked Algebra) http://www.barbaraoakley.com/mfn.html
 
 Available online on rawgit: [https://cdn.rawgit.com/sdenel/HTMLcountdownTimer/50f175361aef0313e82f3a4f522df8cf606339fa/index.html][1]
 
--- inc/script.js
+++ inc/script.js
@@ -1,21 +1,82 @@
+function parseTime(v) {
+	if(v.indexOf(':') == -1) {
+		v = v+':'+'00';
+	}
+	return v;
+}
+var audio = new Audio('inc/66136__theta4__ding30603-spedup.wav');
+
+
+
 var mainApp = angular.module('mainApp', ['ngAnimate']);
 
 mainApp.controller('mainCtrl', function($scope, $http, $timeout) {
-	var hash = window.location.hash.substring(1);
 
-	$scope.timeLengthDisplayed = parseTime((hash != '') ? hash : '55:00');
+    function daemonFunc() {
+	    //console.log("In daemonFunc()");
+	    if($scope.isOn == true) {
+		    remainingTimeMs = $scope.timeLength*1000 - $scope.timeOffset + $scope.dStart-Date.now();
+		    refreshDisplay();
+		    if(remainingTimeMs < 0 && alarmTriggered == false) {
+			    alarmTriggered = true;
+			    ringFunc();
+		    }
+		    $timeout(function() {
+			    daemonFunc();
+		    }, 100);
+	    }
+    }
 
+    function ringFunc() {
+	    if($scope.isOn == true && alarmTriggered == true) {
+		    audio.play();
+		    displayTimeInTitle = !displayTimeInTitle;
+		    $timeout(function() {
+			    ringFunc();
+		    }, 5000);
+	    }
+    }
+
+    function refreshDisplay() {
+	    //console.log("In refreshDisplay()");
+	    var remainingTimeSeconds = Math.abs(remainingTimeMs/1000);
+	    signStr = (remainingTimeMs < 0 ? '-' : '');
 	
+	    minutes = Math.floor(remainingTimeSeconds/60).toString();
+	    minutes = ((minutes.length == 1) ? '0' : '')+minutes
+	    seconds = Math.floor(remainingTimeSeconds%60).toString();
+	    seconds = ((seconds.length == 1) ? '0' : '')+seconds
+	    //console.log(minutes+':'+seconds);
+	    $scope.remainingTime = signStr+minutes+':'+seconds;
+	    $scope.pageTitle = (displayTimeInTitle == true) ? "Chronometer: "+$scope.remainingTime : "Time is up!";
+	    //console.log(alarmTriggered);
 
-	function parseTime(v) {
-		if(v.indexOf(':') == -1) {
-			v = v+':'+'00';
-		}
-		return v;
-	}
+        /*
+         * Set background image
+         */
+        document.getElementsByTagName('html')[0].style.backgroundImage = "url('" + $scope.payload.backgroundImage + "')";
 
-	var audio = new Audio('inc/66136__theta4__ding30603-spedup.wav');
+        /*
+         * Set quotes
+         */
+        var quotesBlock = document.getElementById('quotes');
+        quotesBlock.innerHTML = '';
+        for(var i = 0, I = $scope.payload.quotes.length; i <I; i+=1) {
+            var quote = $scope.payload.quotes[i];
+            quotesBlock.innerHTML += "<h1>"+ quote.title +"</h1>";
+            if(quote.quote !=undefined) quotesBlock.innerHTML += '<p>' + quote.quote + '</p>';
+        }
 
+    }
+
+
+
+	$scope.payload = {}
+    try {
+        $scope.payload = JSON.parse(decodeURIComponent(window.location.hash.substring(1)));
+    } catch(e) {}
+	
+    $scope.displaySettings = false;
 	$scope.isOn = false;
 	$scope.remainingTime = null;
 	$scope.timeOffsetMs = 0; // For hold mode
@@ -25,45 +86,7 @@ mainApp.controller('mainCtrl', function($scope, $http, $timeout) {
 	displayTimeInTitle = true;
 	alarmTriggered = false;
 
-	function daemonFunc() {
-		//console.log("In daemonFunc()");
-		if($scope.isOn == true) {
-			remainingTimeMs = $scope.timeLength*1000 - $scope.timeOffset + $scope.dStart-Date.now();
-			refreshDisplay();
-			if(remainingTimeMs < 0 && alarmTriggered == false) {
-				alarmTriggered = true;
-				ringFunc();
-			}
-			$timeout(function() {
-				daemonFunc();
-			}, 100);
-		}
-	}
 
-	function ringFunc() {
-		if($scope.isOn == true && alarmTriggered == true) {
-			audio.play();
-			displayTimeInTitle = !displayTimeInTitle;
-			$timeout(function() {
-				ringFunc();
-			}, 5000);
-		}
-	}
-
-	function refreshDisplay() {
-		//console.log("In refreshDisplay()");
-		var remainingTimeSeconds = Math.abs(remainingTimeMs/1000);
-		signStr = (remainingTimeMs < 0 ? '-' : '');
-		
-		minutes = Math.floor(remainingTimeSeconds/60).toString();
-		minutes = ((minutes.length == 1) ? '0' : '')+minutes
-		seconds = Math.floor(remainingTimeSeconds%60).toString();
-		seconds = ((seconds.length == 1) ? '0' : '')+seconds
-		//console.log(minutes+':'+seconds);
-		$scope.remainingTime = signStr+minutes+':'+seconds;
-		$scope.pageTitle = (displayTimeInTitle == true) ? "Chronometer: "+$scope.remainingTime : "Time is up!";
-		//console.log(alarmTriggered);
-	}
 
 	$scope.chronoStartStop = function() {
 		if($scope.isOn == true) {
@@ -93,25 +116,74 @@ mainApp.controller('mainCtrl', function($scope, $http, $timeout) {
 		$scope.isOn = false;
 	}
 	
-	$scope.chronoReset = function() {
-		$scope.dStart = Date.now();
-		$scope.timeOffset = 0;
-		remainingTimeMs = $scope.timeLength*1000;
-		displayTimeInTitle = true;
-		alarmTriggered = false;
-		refreshDisplay();
+	$scope.chronoReset = function(noConfirm) {
+        var action = function() {
+            chronoStop();
+            $scope.stopStartLabel = "Start";
+            $scope.timeOffset = 0;
+            remainingTimeMs = $scope.timeLength*1000;
+            displayTimeInTitle = true;
+            alarmTriggered = false;
+
+            /*
+             * Default value for timing + parsing
+             */
+            if($scope.payload.timeLengthDisplayed == undefined) $scope.payload.timeLengthDisplayed = '30:00';
+
+            /*
+             * Default value for quotes
+             */
+            if($scope.payload.quotes == undefined) $scope.payload.quotes = [
+                {title: "Mental contrasting", quote: "Think about where you are now and contrast it with what you want to achieve."},
+                {title: "Focus on process, not product"}
+            ];
+
+            /*
+             * Default value for background
+             */
+            if($scope.payload.backgroundImage == undefined) $scope.payload.backgroundImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Messier51_sRGB.jpg/1280px-Messier51_sRGB.jpg';
+
+            /*
+             * Default value for ask confirm
+             */
+            if($scope.payload.askConfirmReset == undefined) $scope.payload.askConfirmReset = false;
+            $scope.askConfirmReset = $scope.payload.askConfirmReset;
+
+            /*
+             * Default value for stoppable
+             */
+            if($scope.payload.stoppable == undefined) $scope.payload.stoppable = true;
+            $scope.stoppable = $scope.payload.stoppable;
+
+            refreshDisplay();
+        }
+
+        if($scope.askConfirmReset == true && noConfirm != true) {
+            var confirmStatus = confirm("Do you really want to stop and reset the chrono?");
+            if(confirmStatus == true) action();
+        }
+        else action();
 	};
 
 	$scope.chronoSetNewTimeCountAndReset = function() {
-		$scope.timeLengthDisplayed = parseTime($scope.timeLengthDisplayed)
+		$scope.payload.timeLengthDisplayed = parseTime($scope.payload.timeLengthDisplayed);
 
-		var timeLengthDisplayedArr = $scope.timeLengthDisplayed.split(':');
+		var timeLengthDisplayedArr = $scope.payload.timeLengthDisplayed.split(':');
 
-		window.location = window.location.pathname+'#'+$scope.timeLengthDisplayed
+        var obj = 
+		window.location = window.location.pathname+'#'+encodeURIComponent(JSON.stringify($scope.payload));
 		$scope.timeLength = parseInt(timeLengthDisplayedArr[0])*60+parseInt(timeLengthDisplayedArr[1]);
-		$scope.chronoReset();
+		$scope.chronoReset(true);
 	};
 	
+
+    $scope.deleteQuote = function(index) {
+        $scope.payload.quotes.splice(index, 1);
+    }
+
+    $scope.addQuote = function() {
+        $scope.payload.quotes.push({title:"", quote:""});
+    }
 
 	$scope.chronoSetNewTimeCountAndReset();
 	refreshDisplay();
